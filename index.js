7cb1dd574bd941487109e28ec2b1c5a8c647a78b
paulojrge <30898557+paulojrge@users.noreply.github.com>
2018-03-28 19:28:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,95 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-/*var jsonfile = require('jsonfile')
-
-var file = './data/jokes.json';
-var jokes;
-jsonfile.readFile(file, function(err, obj) {
-  console.log("Json file read");
-  jokes = obj;
-})*/
 
 var app = express();
 
 app.use(bodyParser.json());
 
 const port = process.env.port || process.env.PORT || 8000;
+
 app.listen(port, function() {
     console.log('Servidor rodando na porta 8000..');
 });
 
-app.get('/', function(req, res) {  
-    res.send("hello");
-});
+
 
 app.get('/joke', function(req, res) {  
-  //var index = Math.floor(Math.random() * 4);
-  //console.log("joke id: ", index);
-  res.send("hi");
+ res.send("hi");
 });
 
-/*
-app.get('/joke/:jokeid', function(req, res) {
-  console.log("joke id parameter: ", parseInt(req.params.jokeid) - 1);
+Date.prototype.getWeekNumber = function(){
+ var d = new Date();
+ var dayNum = d.getUTCDay() || 7;
+ 
+ d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+ var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+ return Math.ceil((((d - yearStart) / 86400000) + 1)/7)
+};
 
-  var joke = JSON.stringify(jokes[parseInt(req.params.jokeid) - 1]);
-  if(joke){
-    res.send(joke);
-  }else{
-    res.send("Está piada não existe! :/");
-  }
-  
-});*/
+
+var x =   new Date().getWeekNumber() % 4 ;
+
+ var tarefas = [
+              "Lavar a louça do almoço/jantar",
+              "Passar pano na casa & Aspirar tapete",
+              "Secar a louça do almoço/jantar & Fazer gelo",
+              "Pendurar roupas no varal & Tirar o lixo",
+              "Limpeza geral da sala",
+              "Limpeza geral do banheiro",
+              "Nenhuma"
+            ];
+var JSON0 = "{
+    Paulo:tarefas[0],
+    Ana:tarefas[1],
+    Mãe:tarefas[2],
+    Pai:tarefas[3]
+}";
+var JSON1 = "{
+    Paulo:tarefas[3],
+    Ana:tarefas[0],
+    Mãe:tarefas[1],
+    Pai:tarefas[2]
+}";
+var JSON2 = "{
+    Paulo:tarefas[2],
+    Ana:tarefas[3],
+    Mãe:tarefas[0],
+    Pai:tarefas[1]
+}";
+var JSON3 = "{
+    Paulo:tarefas[1],
+    Ana:tarefas[2],
+    Mãe:tarefas[3],
+    Pai:tarefas[0]
+}";
+
+
+switch(x){
+ case 1:
+    app.get('/', function(req,res){
+   res.send(JSON1); 
+});
+                                        
+ break;
+ case 2:
+        app.get('/', function(req,res){
+   res.send(JSON2); 
+});
+                 
+ break;
+ case 3:
+        app.get('/', function(req,res){
+   res.send(JSON3); 
+});
+                    
+ break;
+ case 0:
+        app.get('/', function(req,res){
+   res.send(JSON0); 
+});
+        
+ break;
+                    
+};
 
   
